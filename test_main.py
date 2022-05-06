66bcc2bf09109fcb9e4f77b92368fbe0ac441f4a
Jason Corderoy <jcorderoy@woolworths.com.au>
2022-05-06 10:51:11 +1000

    test cases no longer broken

--- test_main.py
+++ test_main.py
@@ -5,4 +5,3 @@ def test_cleaner():
     assert (mn.cleaner({'text':'A*B(C)D#E%F^'})=='A_B_C_D_E_F_')
     assert (mn.cleaner({'text':'ABCDE'})=='ABCDE')
     assert (mn.cleaner({'text':'%$^&*'})=='_'*5)
-    assert (mn.cleaner({'text':'%$^&*'})=='does not match')
