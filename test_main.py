b2a8e58fb39866512779c51bd22fbd351ec96da5
Jason Corderoy <jcorderoy@woolworths.com.au>
2022-05-06 11:34:26 +1000

    addit test cases

--- test_main.py
+++ test_main.py
@@ -4,4 +4,7 @@ import pytest
 def test_cleaner():
     assert (mn.cleaner({'text':'A*B(C)D#E%F^'})=='A_B_C_D_E_F_')
     assert (mn.cleaner({'text':'ABCDE'})=='ABCDE')
+    assert (mn.cleaner({'text':'ABC123'})=='ABC123')
+    assert (mn.cleaner({'text':'_123___'})=='_123___')
+    assert (mn.cleaner({'text':'---'})=='_'*3)
     assert (mn.cleaner({'text':'%$^&*'})=='_'*5)
