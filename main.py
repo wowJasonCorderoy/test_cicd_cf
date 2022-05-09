4e73c7bc1e0f87fe88db1d936cd63d1f74a6e38f
Jason Corderoy <jcorderoy@woolworths.com.au>
2022-05-10 09:42:16 +1000

    test secret build

--- main.py
+++ main.py
@@ -12,5 +12,15 @@ def cleaner(requests):
     print(clean_str)
     return clean_str
 
+def secret_length(requests):
+    """
+    print length of a secrets value.
+    """
+    env_var_ibm_creds = 'ibm-creds'
+    ibm_creds = json.loads(os.environ[env_var_ibm_creds])
+    url = ibm_creds['url']
+    print(len(url))
+
 if __name__ == "__main__":
-    cleaner()
+    #cleaner()
+    secret_length()
