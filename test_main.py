725bbd6452db64e5f6609e17969c49cbc479164f
Jason Corderoy <jcorderoy@woolworths.com.au>
2022-05-10 10:17:52 +1000

    fix tests

--- test_main.py
+++ test_main.py
@@ -2,9 +2,9 @@ import main as mn
 import pytest
 
 def test_cleaner():
-    assert (mn.cleaner({'text':'A*B(C)D#E%F^'})=='A_B_C_D_E_F_')
-    assert (mn.cleaner({'text':'ABCDE'})=='ABCDE')
-    assert (mn.cleaner({'text':'ABC123'})=='ABC123')
-    assert (mn.cleaner({'text':'_123___'})=='_123___')
-    assert (mn.cleaner({'text':'---'})=='_'*3)
-    assert (mn.cleaner({'text':'%$^&*'})=='_'*5)
+    assert (mn.cleaner({'text':'A*B(C)D#E%F^'})['clean_str']=='A_B_C_D_E_F_')
+    assert (mn.cleaner({'text':'ABCDE'})['clean_str']=='ABCDE')
+    assert (mn.cleaner({'text':'ABC123'})['clean_str']=='ABC123')
+    assert (mn.cleaner({'text':'_123___'})['clean_str']=='_123___')
+    assert (mn.cleaner({'text':'---'})['clean_str']=='_'*3)
+    assert (mn.cleaner({'text':'%$^&*'})['clean_str']=='_'*5)
